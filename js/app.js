710724a7ab73f36ace7a0077ecb12e5a24278c13
LH <lindsaynhenderson@gmail.com>
2022-09-02 21:51:35 -0500

    Write init function and a foreach loop that needs troubleshooting

--- js/app.js
+++ js/app.js
@@ -1,25 +1,54 @@
-// /*-------------------------------- Constants --------------------------------*/
+// /*---------------- Constants --------------------*/
 
 
 
-// /*---------------------------- Variables (state) ----------------------------*/
+// /*---------------- Variables (state) -----------------*/
 
+const board = [null, null, null, null, null, null, null, null, null,]
+//state of the board
+// const turn =
+// //track whose turn it is
+// let winner =
+//represent if anyone has won, or if there is a tie
 
-
-// /*------------------------ Cached Element References ------------------------*/
-const gameSquares = document.querySelector(".gs")
-const squares = document.querySelectorAll("div")
-const squareArray = Array.from(squares)
-const btn = document.querySelector("button")
-////const box = document.querySelector("div")
+// /*------------ Cached Element References -------------*/
+const gameBoard = document.querySelector(".board")
+const squareEls = document.querySelectorAll(".gs")
 const resetB = document.querySelector("#reset-button")
 const messageEl = document.querySelector("#message")
 
-// /*----------- Event Listeners -----------------*/
+
+// /*----------- Event Listeners ----------------*/
 
 messageEl.addEventListener('click', handleClick)
-gameSquares.addEventListener('click', handleClick)
+//squareEls.addEventListener('click', handleClick)
 // /*-------------- Functions ---------------*/
+
+function init() {
+    messageEl.textContent = "Player 1 select a square!"
+    console.log(messageEl)
+    let board = [null, null, -1, null, null, null, 1, null, null,]
+    turn = 1
+    winner = null
+    console.log(board)
+    render()
+}
+
+function render() { 
+    //display updated state as changes are made
+board.forEach(function(element, index) {
+    if (element === 1){
+        squareEls[index].textContent = 'X'
+    } else if (element === -1){
+        squareEls[index].textContent = "O"
+        }
+        console.log(element)
+        console.log(board[index])
+        console.log(squareEls[index])
+    })
+    }
+
+    init()
 
 function handleClick(){
     console.log("you got this!")
@@ -29,19 +58,25 @@ function handleClick(){
 
 // 2) Store cached element references
 
-// 3) Upon loading, the game state should be initialized, and a function should be 
-//    called to render this game state
-init()
-console.log(messageEl)
-console.log(squares)
-console.log(squareArray) 
-
-
+// 3) Upon loading, the game state should be initialized, and a function should be called to render this game state
 
 
 // 4) The state of the game should be rendered to the user
 
+
 // 5) Define the required constants
+
+winningCombos = [
+[0, 1, 2],
+[3, 4, 5],
+[6, 7, 8],
+[0, 4, 8],
+[2, 4, 6]
+]
+
+//***********************************************
+
+
 
 
 
@@ -50,19 +85,7 @@ console.log(squareArray)
 
 
 
-function init() {
-messageEl.textContent = "Player 1 select a square!"
-}
 
-function render() {
-
-}
-//display updated state as changes are made
-//* squareArray = div#sq0, div#sq1, div#sq2, div#sq3, div#sq4, div#sq5, div#sq6, div#sq7, div#sq8]
-
-squareArray.forEach(square => {
-    square= 'null'
-})
 
 
 
@@ -76,17 +99,5 @@ squareArray.forEach(square => {
 
 // //? 8) Create Reset functionality
 
-// function init() {
-//     isWinner = false
-//     squares = null
-//     messageEl.innerText = "Player One, select a square"
-// }
 
 
-// winningCombos = [
-//     [0, 1, 2],
-//     [3, 4, 5],
-//     [6, 7, 8],
-//     [0, 4, 8],
-//     [2, 4, 6]
-//   ]
