647ba1feb5de7cbea462ed35dc7b7a682cfa1cde
LH <lindsaynhenderson@gmail.com>
2022-09-04 13:29:36 -0500

    handleclick and render are both working

--- js/app.js
+++ js/app.js
@@ -4,19 +4,19 @@
 
 // /*---------------- Variables (state) -----------------*/
 
-//const board = [null, null, null, null, null, null, null, null, null,]
 //state of the board
 // const turn =
 // //track whose turn it is
 // let winner =
 //represent if anyone has won, or if there is a tie
-winningCombos = [
+const winningCombos = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
     [0, 4, 8],
     [2, 4, 6]
     ]
+let board = [null, null, null, null, null, null, null, null, null]
 
 // /*------------ Cached Element References -------------*/
 const gameBoard = document.querySelector(".board")
@@ -32,7 +32,10 @@ gameBoard.addEventListener('click', handleClick)
 // /*-------------- Functions ---------------*/
 
 function handleClick(evt) {
-    console.log(evt.target.id)
+    sId = parseInt(evt.target.id[2])
+board.splice(sId, 1, turn)
+console.log(board)
+render()
 }
 
 function init() {
@@ -45,36 +48,38 @@ function init() {
     render()
 }
 
-const board = [-1, 1, null, 1, -1, null, null, null, -1]
 
+function render() {
 
-function render() { 
-board.forEach(function(_num, index) {
-    // if (_num != null){
+board.forEach(function(object, index) {
     //     messageEl.textContent = "This square is already occupied.  Try a different one"
     // }
-    if (_num === 1){
-        squareEls[index].textContent = 'X'
-    } else if (_num === -1){
-        squareEls[index].textContent = "O"
-    } else {
-        squareEls[index].textContent = null
-    }
+console.log(board)
+    if 
+    (object === 1)
+        {squareEls[index].textContent = 'X'
+} else if ( object=== -1) {
+    squareEls[index].textContent = "O"
+} else {
+    squareEls[index].textContent = null
+}
+})}
         
-        console.log(_num)
-        console.log(board[index])
-        console.log(squareEls[index])
-    })
-// if winner = null, switch between player 1 and 2
-if (winner === null) { 
-    return (turn === 1 ? messageEl.textContent = `Player 1, it's your turn`
-    : messageEl.textContent = `Player 2, it's your turn`)
-} else if (winner === "T") {
-    return messageEl.textContent = "It's a tie game!"
-}   else {
-    return (winner === 1 ? messageEl.textContent = "player 1 wins!" : messageEl.textContent = "player 2 wins!")
-}
-}
+//console.log(object)
+//         console.log(board[index])
+//         console.log(squareEls[index])
+
+// // if winner = null, switch between player 1 and 2
+
+// if (winner === null) { 
+//     return (turn === 1 ? messageEl.textContent = `Player 1, it's your turn`
+//     : messageEl.textContent = `Player 2, it's your turn`)
+// } else if (winner === "T") {
+//     return messageEl.textContent = "It's a tie game!"
+// }   else {
+//     return (winner === 1 ? messageEl.textContent = "player 1 wins!" : messageEl.textContent = "player 2 wins!")
+// }
+// }
 init()
 
 
