67df353a421e503ec1e28e6774ad37f648713d31
LH <lindsaynhenderson@gmail.com>
2022-09-05 10:58:22 -0500

    getWinner is working

--- js/app.js
+++ js/app.js
@@ -44,16 +44,20 @@ function resetClick() {
 
 function handleClick(evt) {
     sqIdx = parseInt(evt.target.id[2])
-    board.splice(sqIdx, 1, turn)
+if (board[sqIdx] != null) {
+    return messageEl.textContent = "occupied, try a different square"}
+    else {
+    board.splice(sqIdx, 1, turn)}
     console.log(sqIdx)
-    
-    if (board[sqIdx] != null)
-    {messageEl.textContent = "occupied, try a different square"}
-    //if (winner != null)
-    turn = (turn * -1)
-    console.log(turn)
+
+    // //if (winner != null)
+    // else {}
+
     getWinner()
-    render()
+    console.log('winning')
+
+
+render()
 }
 
 function render() {
@@ -68,6 +72,7 @@ board.forEach(function(object, index) {
     } else {
     squareEls[index].textContent = null}
 })
+
     if (turn === -1) {player = "Player 2"}
     else {player = "Player 1"}
 
@@ -77,9 +82,19 @@ board.forEach(function(object, index) {
     messageEl.textContent = "It's a tie! Try again"
 } else {messageEl.textContent = `Great job ${player}, you win!`
 }
+turn = (turn * -1)
 }
 
 function getWinner() {
+winningCombos.forEach(function(combo) {
+    sum = Math.abs(board[combo[0]] + board[combo[1]] + board[combo[2]])
+    if (sum === 3) {
+        winner = turn
+    } else if (board.includes(!null)) {
+        winner = "T"}
+    })} 
+console.log(winner + " is winner!")
+
 //     for (let i = 0; i < winningCombos.length; i++){
 //         sum = 
 //         board[winningCombos[i][0]] + 
@@ -92,7 +107,3 @@ function getWinner() {
 //     else if (board.includes(null) === false) {winner = "T"}
 //     else { winner = null
 // }   
-    
-    console.log(winner + " is winner!")
-}
-    
