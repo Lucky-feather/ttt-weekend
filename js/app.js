98d3d6a193436bc7ada1655166474467768a899a
LH <lindsaynhenderson@gmail.com>
2022-09-06 10:59:18 -0500

    cleaning code

--- js/app.js
+++ js/app.js
@@ -1,9 +1,5 @@
 // /*---------------- Constants --------------------*/
 
-
-
-// /*---------------- Variables (state) -----------------*/
-
 const winningCombos = [
     [0, 1, 2],
     [3, 4, 5],
@@ -14,30 +10,26 @@ const winningCombos = [
     [1, 4, 7],
     [2, 5, 8]
     ]
+    // /*---------------- Variables (state) -----------------*/
 let board = [null, null, null, null, null, null, null, null, null]
 let turn 
 let winner 
-
 // /*------------ Cached Element References -------------*/
 const gameBoard = document.querySelector(".board")
 const squareEls = document.querySelectorAll(".gs")
 const resetBtnEl = document.querySelector("#reset-button")
 const messageEl = document.querySelector("#message")
-
 // /*----------- Event Listeners ----------------*/
-
 resetBtnEl.addEventListener('click', resetClick)
 gameBoard.addEventListener('click', handleClick)
 // /*-------------- Functions ---------------*/
 
 function init() {
     messageEl.textContent = "Player 1 select a square!"
-    console.log(messageEl)
     board = [null, null, null, null, null, null, null, null, null]
     turn = 1
     winner = null
     player = "player 1"
-    
     render()
 }
 
@@ -49,28 +41,22 @@ function resetClick() {
 
 function handleClick(evt) {
     sqIdx = parseInt(evt.target.id[2])
-if (board[sqIdx] != null) {
-    return messageEl.textContent = "occupied, try a different square"}
+    if (board[sqIdx] != null) {
+    return messageEl.textContent = "occupied, try a different square"
+    }
     else if (winner != null) {
-        return
+    return
     }
     else {
-    board.splice(sqIdx, 1, turn)}
-    console.log(sqIdx)
-
+    board.splice(sqIdx, 1, turn)
+    }
     getWinner()
-    console.log('winning')
     turn = (turn * -1)
 render()
-console.log("switch players")
-
-
 }
 
 function render() {
-    console.log(board)
 board.forEach(function(object, index) {
-    
     if 
     (object === 1)
         {squareEls[index].textContent = 'X'
@@ -101,23 +87,10 @@ function getWinner() {
 winningCombos.forEach(function(combo) {
     sum = Math.abs(board[combo[0]] + board[combo[1]] + board[combo[2]])
     if (sum === 3) {
-        winner = turn
-    } else if ((board.includes(null) === false) && (winner === null)) {
-        console.log(winner)
-        winner = "T"}
+    winner = turn
+}
+    else if ((board.includes(null) === false) && (winner === null)) {
+    winner = "T"
+}
     })} 
 
-console.log(winner + " is winner!")
-
-//     for (let i = 0; i < winningCombos.length; i++){
-//         sum = 
-//         board[winningCombos[i][0]] + 
-//         board[winningCombos[i][1]] +
-//         board[winningCombos[i][2]]
-//     }
-//     console.log (sum)
-//     if (sum === 3) {winner = 1}
-//     if (sum === -3) {winner = -1}
-//     else if (board.includes(null) === false) {winner = "T"}
-//     else { winner = null
-// }   
